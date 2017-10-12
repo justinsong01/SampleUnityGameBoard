0ce04818c1ff53a120b0403ef50523e39123eb48
Justin Song <yjsong0404@gmail.com>
2017-10-12 11:25:58 -0400

    Pushing the Unity projects from the local
    pushing all relevant unity gameboard to github

---
+++ CardGame/Assets/Scripts/Draggable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	public Transform originalParent = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnBeginDrag(PointerEventData eventData) {
+		Debug.Log ("Begin Dragging");
+		originalParent = this.transform.parent;
+		this.transform.SetParent (this.transform.parent.parent);
+
+		GetComponent<CanvasGroup> ().blocksRaycasts = false;
+	}
+
+	public void OnDrag (PointerEventData eventData) {
+		//Debug.Log ("Dragging");
+		this.transform.position = eventData.position;
+	}
+
+	public void OnEndDrag(PointerEventData eventData) {
+		Debug.Log ("Done Dragging");
+		this.transform.SetParent (originalParent);
+		//GetComponent<CanvasGroup> ().blocksRaycasts = true;
+
+		//EventSystem.current.RaycastAll (eventData);
+	}
+}
---
+++ CardGame/Assets/Scripts/DropArea.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropArea : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnPointerEnter(PointerEventData eventData) {
+		Debug.Log ("On Pointer entering");
+	}
+
+	public void OnPointerExit(PointerEventData eventData) {
+		Debug.Log ("On Pointer EXIT");
+	}
+
+	public void OnDrop(PointerEventData eventData) {
+
+		Debug.Log (eventData.pointerDrag.name + "was dropped on " + gameObject.name);
+		Draggable sample = eventData.pointerDrag.GetComponent<Draggable> ();
+		if (sample != null) {
+			sample.originalParent = this.transform;
+		}
+	}
+
+}
